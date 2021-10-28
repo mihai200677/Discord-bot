7e0226fcfbe1b20371698057c078e341b52b42b3
mihai200677 <69989935+mihai200677@users.noreply.github.com>
2021-10-28 23:14:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,27 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
-  console.log(`Loggined as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
 
-client.login('ur token here');
+
+// WORKING STATUS FOR PUT DOWN: STREAMING, WATCHING, CUSTOM_STATUS, PLAYING, COMPETING
+client.on('ready', () => {
+  client.user.setActivity('PUT STATUS HERE',{ type: 'PLAYING' });
+   console.log("Activity set work")
+    });
+
+
+//HERE YOU CAN SET IDLE ETC
+client.on('ready', () => {
+  client.user.setStatus('online');
+    console.log("Status work")
+    });
+
+
+
+
+
+client.login('Ur token here');
