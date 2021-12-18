e5d2028ec332de946e951e9c947df389bb64dbfe
mihai200677 <69989935+mihai200677@users.noreply.github.com>
2021-12-18 12:01:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('_____________________________________')
+  console.log('|')
+  console.log('|')
+  console.log(`| Logged in as ${client.user.tag}!`);
+  console.log('|')
+  console.log('|')
+  console.log('_____________________________________')
 });
 
 
 
 // WORKING STATUS FOR PUT DOWN: STREAMING, WATCHING, CUSTOM_STATUS, PLAYING, COMPETING
 client.on('ready', () => {
-  client.user.setActivity('PUT STATUS HERE',{ type: 'PLAYING' });
+  client.user.setActivity('Made by Made by mihai ᴷˣ#4328',{ type: 'PLAYING' });
    console.log("Activity set work")
-    });
+});
 
 //HERE YOU CAN SET IDLE ETC
 client.on('ready', () => {
